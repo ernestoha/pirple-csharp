f509804be52a2c511b659cf72397ba27bb439799
Ernesto Herrera <ernestoha@gmail.com>
2020-04-26 15:02:35 -0400

    Homework Assignment #4: Improvment Assignemt #1 with new vars.

--- animal.cs
+++ animal.cs
@@ -14,19 +14,29 @@ namespace Rextester
         {
 			//Vars
 			string animal = "Lion";
-			byte feet = 4;
+			byte legs = 4;
 			float heigthFeet = 3.9F;
 			int weightPound = 303;           
 			char firstLetter = 'L';
 			bool felidae = true;
 			string says = "Grrrr";
+			string scientificName = "Felis Leo";
+			string[] colors = new string[2]{ "orange", "yellow" };
+			string[] habitat = new string[2]{ "Africa", "Asia" };
 			
 			//Output
-			Console.WriteLine("animal: {0:s}\nfeet: {1:d}", animal, feet);
+			Console.WriteLine("animal: {0:s}\nlegs: {1:d}", animal, legs);
 			Console.WriteLine("heigthFeet: {0:f2}\nweightPound: {1:d}", heigthFeet, weightPound);
 			Console.WriteLine("firstLetter: {0:c}\nfelidae: {1:b}", firstLetter, felidae);
-			Console.WriteLine("says: {0:s}", says);
-       		
+			Console.WriteLine("says: {0:s}\nscientificName: {1:s}", says, scientificName);
+			Console.WriteLine("colors:");
+			foreach(var item in colors){
+				Console.WriteLine(" * {0:s}", item);
+			}
+       		Console.WriteLine("habitat:");
+			foreach(var item in habitat){
+				Console.WriteLine(" * {0:s}", item);
+			}
 			// Console.ReadLine();
         }
     }
