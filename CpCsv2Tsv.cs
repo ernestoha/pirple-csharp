d10b2d71c1ef5aec3c36013263f11bb0075f8891
Ernesto Herrera <ernestoha@gmail.com>
2020-05-26 20:51:47 -0400

    Homework Assignment #10: Handle Exceptions Copy the data from csv to tsv.

--- CpCsv2Tsv.cs
+++ CpCsv2Tsv.cs
@@ -7,45 +7,108 @@ namespace Test001
 	public class CpCsv2Tsv
 	{
 
-		static StringBuilder ReadFile2Tsv001(string fileNameFrom)
+		public class IOResponse
 		{
-			StringBuilder tsvText = new StringBuilder();
-			const Int32 BufferSize = 128;
-			using (var fileStream = File.OpenRead(fileNameFrom))
-			using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize)) {
-				String line;
-				while ((line = streamReader.ReadLine()) != null)
-				{
-					// Process line
-					// Console.WriteLine(line);
-
-					tsvText.AppendLine(line.Replace(",", "\t"));
-				}
+			public bool IsOk { get; private set;} 
+			public String Response { get; private set;} 
+			public StringBuilder TsvText { get; private set;} 
+			public IOResponse(bool isOk, String response, StringBuilder tsvText)
+			{
+				this.IsOk = isOk;
+				this.Response = response;
+				this.TsvText = tsvText;
 			}
-			return tsvText;
+			public IOResponse(bool isOk, String response)
+			{
+				this.IsOk = isOk;
+				this.Response = response;
+			}
 		}
 
-		static void WriteFile(string fileNameTo, StringBuilder tsvText)
+		static IOResponse ReadFile2Tsv001(string fileNameFrom)
 		{
-			
+			bool isOk = false;
+			String msg = null;
+			StringBuilder tsvText = new StringBuilder();
+			const Int32 BufferSize = 128;
+			try
+			{
+				using (var fileStream = File.OpenRead(fileNameFrom))
+				using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize)) {
+					String line;
+					while ((line = streamReader.ReadLine()) != null)
+					{
+						// Process line
+						// Console.WriteLine(line);
+
+						tsvText.AppendLine(line.Replace(",", "\t"));
+					}
+				}
+				isOk = true;
+				msg = "Ok";
+			}
+			catch(FileNotFoundException)
+			{
+				msg = fileNameFrom+" File do not exists.";// + e.Message;
+			}
+			catch(IOException e)
+			{
+				msg = e.GetType().Name+": The write operation could not " +
+					"be performed because the specified " +
+					"part of the file is locked.";
+			}
+			catch(Exception e)
+			{
+				msg = e.GetType().Name+": Exception. "+ e.Message;
+			}
+			return new IOResponse(isOk, msg, tsvText);
+		}
+
+		static IOResponse WriteFile(string fileNameTo, StringBuilder tsvText)
+		{
+			bool isOk = false;
+			String msg = null;	
 			// Check if file already exists. If yes, delete it.
 			if (File.Exists(fileNameTo))
 			{
 				File.Delete(fileNameTo);
 			}
-			using(var streamWriter = new StreamWriter(fileNameTo)) {
-				streamWriter.Write(tsvText.ToString());
+			try
+			{
+				using(var streamWriter = new StreamWriter(fileNameTo)) {
+					streamWriter.Write(tsvText.ToString());
+				}
+				isOk = true;
+				msg = "Ok";
 			}
+			catch(Exception e)
+			{
+				msg = e.GetType().Name+": Exception. "+ e.Message;
+			}
+			return new IOResponse(isOk, msg);
 		}
 
 		static void Main() {
             //https://www.kaggle.com/karangadiya/fifa19
 			string fileNameFrom = @"data.csv";
 			string fileNameTo = @"fifa-tab.tsv";
-			WriteFile(fileNameTo, ReadFile2Tsv001(fileNameFrom));
+			// WriteFile(fileNameTo, ReadFile2Tsv001(fileNameFrom)); //old v. without handle exception
 
-			Console.WriteLine("------------------------------");
+            IOResponse resRead = ReadFile2Tsv001(fileNameFrom);
+			IOResponse resWrite = (resRead.IsOk) ? WriteFile(fileNameTo, resRead.TsvText) : new IOResponse(false, "File CSV was not copied.");
+			
+			//Output
+			Console.WriteLine("--------------READ-BEGIN---------------");
+			Console.WriteLine("isOk = {0}",resRead.IsOk);
+			Console.WriteLine("Response = {0}",resRead.Response);
+			Console.WriteLine("---------------READ-END----------------\n\n");
+
+			Console.WriteLine("--------------WRITE-BEGIN--------------");
+			Console.WriteLine("isOk = {0}",resWrite.IsOk);
+			Console.WriteLine("Response = {0}",resWrite.Response);
+			Console.WriteLine("---------------WRITE-END---------------\n\n");
+			
+			Console.WriteLine("---------------------------------------");
 		}
 	}
 }
-
